189ed031ffc49c2ca1b804835a7f47336a40f596
Arthur Carminatti <arthurcarminattigit@gmail.com>
2023-04-03 16:11:17 -0300

    Starting styling created tasks

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -2,6 +2,8 @@ import igniteNoItensList from '../assets/ignite-no-itens-list.svg';
 
 import styles from '../css/Task.module.css'
 
+import { TbTrash } from "react-icons/tb";
+
 export function Task() {
     return (
         <div className={styles.tasks}>
@@ -9,11 +11,13 @@ export function Task() {
                 <div className={styles.created}>
 
                     <p className={styles.pTasks}>Tarefas Criadas</p>
-                    
+                    <span>10</span>
+
                 </div>
 
                 <div className={styles.done}>
                     <p>Concluídas</p>
+                    <span>2 de 10</span>
 
                 </div>
             </div>
@@ -23,6 +27,18 @@ export function Task() {
                 <div className={styles.text}>
                     <strong>Você ainda não tem tarefas cadastradas</strong>
                     <p>Crie tarefas e organize seus itens a fazer</p>
+                </div>
+            </div>
+
+            <div >
+                <div className={styles.taskCreated}>
+                    <button className={styles.checkContainter}>
+                        <div />
+                    </button>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere corporis nisi similique iusto expedita saepe unde inventore quis odio, possimus perferendis.</p>
+                    <button className={styles.deleteButton}>
+                        <TbTrash size={20} />
+                    </button>
                 </div>
             </div>
         </div>
