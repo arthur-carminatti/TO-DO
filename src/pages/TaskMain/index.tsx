7eec6bc2ad11882105eaf5ac3d7aa7bd236dd3b6
Arthur Carminatti <arthurcarminattigit@gmail.com>
2023-04-26 12:49:07 -0300

    Styling the input and button

--- src/pages/TaskMain/index.tsx
+++ src/pages/TaskMain/index.tsx
@@ -1,10 +1,21 @@
 import igniteLogo from '../../assets/ignite-logo.svg'
-import { TaskMainHeaderContainer } from "./styles";
+import { PlusCircle } from 'phosphor-react';
+
+import { TaskMainHeaderContainer, TaskMainInputContainer } from "./styles";
+
 
 export function TaskMain() {
     return (
-        <TaskMainHeaderContainer>
-            <img src={igniteLogo} alt="" />
-        </TaskMainHeaderContainer>
+        <>
+            <TaskMainHeaderContainer>
+                <img src={igniteLogo} alt="" />
+            </TaskMainHeaderContainer>
+
+            <TaskMainInputContainer>
+                <input type="text" placeholder='Adicione uma nova tarefa'/>
+                <button>Criar <PlusCircle size={20}/></button>
+            </TaskMainInputContainer>
+        </>
+
     )
 }
