9cfebdcfd4fc75305cbb3e5193a71faad15ee58d
Arthur Carminatti <arthurcarminattigit@gmail.com>
2023-04-26 13:53:30 -0300

    Creating the tasks layout

--- src/pages/TaskMain/index.tsx
+++ src/pages/TaskMain/index.tsx
@@ -1,7 +1,17 @@
-import igniteLogo from '../../assets/ignite-logo.svg'
-import { PlusCircle } from 'phosphor-react';
+import {
+    TaskMainEmptyTaskContainer,
+    TaskMainHeaderContainer,
+    TaskMainInfoCreatedContainer,
+    TaskMainInfoDoneContainer,
+    TaskMainInfoTaskContainer,
+    TaskMainInputContainer,
+    TaskMainTaskContainer
+} from "./styles";
 
-import { TaskMainHeaderContainer, TaskMainInputContainer } from "./styles";
+import igniteLogo from '../../assets/ignite-logo.svg'
+import taskEmptyLogo from '../../assets/ignite-no-itens-list.svg'
+
+import { PlusCircle } from 'phosphor-react';
 
 
 export function TaskMain() {
@@ -12,9 +22,31 @@ export function TaskMain() {
             </TaskMainHeaderContainer>
 
             <TaskMainInputContainer>
-                <input type="text" placeholder='Adicione uma nova tarefa'/>
-                <button>Criar <PlusCircle size={20}/></button>
+                <input type="text" placeholder='Adicione uma nova tarefa' />
+                <button>Criar <PlusCircle size={20} /></button>
             </TaskMainInputContainer>
+
+            <TaskMainTaskContainer>
+                <TaskMainInfoTaskContainer>
+                    <TaskMainInfoCreatedContainer>
+                        <p>Tarefas criadas</p>
+                        <span>0</span>
+                    </TaskMainInfoCreatedContainer>
+
+                    <TaskMainInfoDoneContainer>
+                        <p>Concluídas</p>
+                        <span>0</span>
+                    </TaskMainInfoDoneContainer>
+                </TaskMainInfoTaskContainer>
+
+                <TaskMainEmptyTaskContainer>
+                    <img src={taskEmptyLogo} alt="" />
+                    <p>
+                        <span>Você ainda não tem tarefas cadastradas</span>
+                        <br /> Crie tarefas e organize seus itens a fazer
+                    </p>
+                </TaskMainEmptyTaskContainer>
+            </TaskMainTaskContainer>
         </>
 
     )
--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -1,11 +1,14 @@
 export const defaultTheme = {
     'gray-300': '#808080',
+    'gray-400': '#333333',
     'gray-500': '#262626',
     'gray-600': '#1A1A1A',
     'gray-700': '#0D0D0D',
 
-    'blue': '#4EA8DE',
+    '-purple': '#8284FA',
+
+    '-blue': '#4EA8DE',
     'blueDark': '#1E6F9F',
 
-    'white': '#FFF',
+    '-white': '#FFF',
 }
