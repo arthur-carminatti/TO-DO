353aafa1aa1432959e2c6b706824db3865d4d091
Arthur Carminatti <arthurcarminattigit@gmail.com>
2023-05-01 15:15:03 -0300

    remove textarea

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -17,7 +17,7 @@ export const GlobalStyle = createGlobalStyle`
         -webkit-font-smoothing: antialiased;
     }
 
-    body, input, textarea, button {
+    body, input, button {
         font-family: 'Roboto', sans-serif;
         font-weight: 400;
         font-size: 1rem;
